6e8e938030451cc3046b67aa4f74510118d10144
Wufuwei <fu-wei.wu@qq.com>
2019-09-22 22:42:44 +0800

    初始化我的项目

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// 项目入口文件
+import Vue from 'vue'
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+
+// 按需导入Mint-UI中的组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name,Header)
+
+// 导入App根组件
+import app from './App.vue'
+
+var vm = new Vue({
+    el:'#app',
+    render:c=>c(app)
+})
